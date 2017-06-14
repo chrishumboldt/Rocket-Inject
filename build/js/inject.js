870fff4feea5377192791203d4406a5140a8c3a1
Chris Humboldt <chrismodem@gmail.com>
2017-06-14 08:32:07 +0200

    Small changes.

--- build/js/inject.js
+++ build/js/inject.js
@@ -1,23 +1,38 @@
+/*
+Author: Chris Humboldt
+*/
+// Rocket module extension
 Rocket.defaults.inject = {
     errors: true
 };
+// Module container
 var RockMod_Inject;
 (function (RockMod_Inject) {
+    // Variables
     var components = {};
+    /*
+    All methods pertaining to the components themselves exist here.
+    This object acts as the methods namespace.
+    */
     var componentMethods = {
         bind: function (obj) {
+            // Catch
             if (!validate.bind(obj)) {
                 return false;
             }
             ;
+            // Continue
             var listBindTo = (Rocket.is.string(obj.to)) ? Rocket.dom.select(obj.to) : Rocket.dom.select('#' + obj.component)[0];
+            // Catch
             if (!Rocket.exists(listBindTo)) {
                 return false;
             }
+            // Continue
             var data = (Rocket.is.object(obj.data)) ? obj.data : '';
             var html = Mustache.render(components[obj.component].html, data);
             for (var _i = 0, listBindTo_1 = listBindTo; _i < listBindTo_1.length; _i++) {
                 var bindTo = listBindTo_1[_i];
+                // Overwrite or append
                 if (obj.overwrite === true) {
                     bindTo.innerHTML = html;
                 }
@@ -25,24 +40,30 @@ var RockMod_Inject;
                     bindTo.insertAdjacentHTML('beforeend', html);
                 }
                 bindTo.setAttribute('data-inject', 'true');
+                // Set an id on the container (bindTo element)
                 if (Rocket.is.string(components[obj.component].id)) {
                     bindTo.id = components[obj.component].id;
                 }
+                // Set a class on the container (bindTo element)
                 if (Rocket.is.string(components[obj.component].className)) {
                     Rocket.classes.add(bindTo, components[obj.component].className);
                 }
+                // Component onDone function
                 if (Rocket.is.function(components[obj.component].onDone)) {
                     components[obj.component].onDone(bindTo);
                 }
+                // Binding onDone function
                 if (Rocket.is.function(obj.onDone)) {
                     obj.onDone(bindTo);
                 }
             }
         },
         generate: function (obj) {
+            // Catch
             if (!validate.generate(obj)) {
                 return false;
             }
+            // Continue
             var html = '';
             if (Rocket.is.object(obj.data)) {
                 html = Mustache.render(components[obj.component].html, obj.data);
@@ -50,12 +71,14 @@ var RockMod_Inject;
             return html;
         },
         register: function (obj) {
+            // Catch
             if (!validate.register(obj)) {
                 if (Rocket.defaults.errors) {
                     throw new Error('Injectplate: Please provide a valid component name.');
                 }
                 return false;
             }
+            // Continue
             components[obj.name] = {
                 className: (Rocket.is.string(obj.className)) ? obj.className : false,
                 id: (Rocket.is.string(obj.id)) ? obj.id : false,
@@ -65,6 +88,9 @@ var RockMod_Inject;
             };
         }
     };
+    /*
+    Validation methods are used to check all incoming data.
+    */
     var validate = {
         bind: function (obj) {
             if (!Rocket.is.object(obj)) {
@@ -97,6 +123,10 @@ var RockMod_Inject;
             return true;
         }
     };
+    /*
+    All templates are flattened for convenience and to make sure there are no
+    unnecessary spaces.
+    */
     function flattenHTML(html, name) {
         var htmlFlat = '';
         switch (typeof html) {
@@ -113,10 +143,12 @@ var RockMod_Inject;
                 }
                 return htmlFlat;
         }
+        // Type check was unsuccessful
         if (Rocket.defaults.errors) {
             throw new Error('Injectplate: The HTML provided to create the component "' + name + '" is not valid.');
         }
     }
+    // Exports
     RockMod_Inject.bind = componentMethods.bind;
     RockMod_Inject.component = componentMethods.register;
     RockMod_Inject.flatten = flattenHTML;
